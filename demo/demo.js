90e4bc5f6d770f2b7ca8d74f4fab11f23a0458f3
William Grasel <william.grasel@ifood.com.br>
2022-02-19 17:07:16 -0300

    docs(demo): general refactor with better user messages and code organization

--- demo/demo.js
+++ demo/demo.js
@@ -2,8 +2,106 @@ const { CryptoStorage } = require('../dist/web-crypto-storage.cjs');
 
 let cryptoStorage;
 
-const INITIAL_NOTES = `You can insert any text you want here.
-Save it to storage and reload the page.`;
+const Actions = {
+  async login() {
+    try {
+      const user = value('user');
+      let pass = value('pass');
+      const logintype = value('logintype');
+
+      if (!user || (logintype !== 'webauthn' && !pass)) {
+        return updateStatus('User and password are required');
+      }
+
+      if(logintype !== 'password') {
+        pass = await webauthn(user, logintype === 'webauthn' ? user : pass)
+      }
+
+      // Here the user password is used as base crypto key
+      // and the user name is used as database name to fully scope the data:
+      cryptoStorage = new CryptoStorage(pass, user);
+      const notes = await cryptoStorage.get('mynotes');
+
+      if (notes) {
+        value('text', notes);
+        updateStatus('Notes loaded for ' + user);
+      } else {
+        value('text', 'You can insert any text you want here.');
+        updateStatus('No saved notes found for ' + user);
+      }
+
+      setclass('loginform', 'hide');
+      setclass('notes', 'show');
+      focus('text');
+    } catch (error) {
+      updateStatus(error);
+    }
+  },
+
+  load() {
+    cryptoStorage.get('mynotes').then(notes => {
+      if (notes) {
+        value('text', notes);
+        updateStatus('Notes loaded.');
+      } else {
+        updateStatus('No saved notes found!');
+      }
+    }, updateStatus);
+  },
+
+  save() {
+    const notes = value('text');
+    cryptoStorage.set('mynotes', notes).then(() => {
+      updateStatus('Notes saved.');
+    });
+  },
+
+  clear() {
+    cryptoStorage.clear();
+    value('text', '');
+    updateStatus('Storage cleared, just crypto salt remains.');
+  },
+
+  logout() {
+    value('user', '');
+    value('pass', '');
+    cryptoStorage.close();
+    setclass('loginform', 'show');
+    setclass('notes', 'hide');
+    updateStatus('Logged out.');
+    focus('user');
+  },
+};
+
+addEventListener('click', event => {
+  if(event.target.type !== 'button' && event.target.type !== 'submit') {
+    return;
+  }
+
+  event.preventDefault();
+  switch (event.target.id) {
+    case 'login':
+      Actions.login();
+      break;
+    case 'load':
+      Actions.load();
+      break;
+    case 'save':
+      Actions.save();
+      break;
+    case 'clear':
+      Actions.clear();
+      break;
+    case 'logout':
+      Actions.logout();
+      break;
+  }
+});
+
+addEventListener('submit', event => {
+  event.preventDefault();
+  Actions.login();
+});
 
 function value(id, val) {
   let input = document.getElementById(id) || Array.from(document.getElementsByName(id)).find(e => e.checked);
@@ -15,7 +113,7 @@ function value(id, val) {
 }
 
 function focus(id) {
-  document.getElementById(id).focus();
+  document.getElementById(id).select();
 }
 
 function setclass(id, val) {
@@ -60,112 +158,3 @@ async function webauthn(user, challenge) {
 
   return clientDataObj.challenge;
 }
-
-const Buttons = {
-  async login() {
-    try {
-      const user = value('user');
-      let pass = value('pass');
-      const logintype = value('logintype');
-
-      if (!user || (logintype !== 'webauthn' && !pass)) {
-        return updateStatus('User and password are required');
-      }
-
-      if(logintype !== 'password') {
-        pass = await webauthn(user, logintype === 'webauthn' ? user : pass)
-      }
-
-      // Here the user password is used as base crypto key
-      // and the user name is used as database name to fully scope the data:
-      cryptoStorage = new CryptoStorage(pass, user);
-      const notes = await cryptoStorage.get('mynotes');
-
-      if (notes) {
-        value('text', notes);
-        updateStatus('Notes loaded for ' + user);
-      } else {
-        Buttons.paste();
-        updateStatus('Example notes pasted. No saved notes found for ' + user);
-      }
-
-      setclass('loginform', 'hide');
-      setclass('notes', 'show');
-    } catch (error) {
-      updateStatus(error);
-    }
-  },
-
-  load() {
-    cryptoStorage.get('mynotes').then(notes => {
-      if (notes) {
-        value('text', notes);
-        updateStatus('Notes loaded.');
-      } else {
-        updateStatus('No saved notes found!');
-      }
-    }, updateStatus);
-  },
-
-  save() {
-    const notes = value('text');
-    cryptoStorage.set('mynotes', notes).then(() => {
-      updateStatus('Notes saved.');
-    });
-  },
-
-  paste() {
-    value('text', INITIAL_NOTES);
-    updateStatus('Example notes pasted.');
-  },
-
-  clear() {
-    cryptoStorage.clear();
-    Buttons.paste();
-    updateStatus('Example notes pasted. (Storage cleared.)');
-  },
-
-  logout() {
-    value('user', '');
-    value('pass', '');
-    cryptoStorage.close();
-    setclass('loginform', 'show');
-    setclass('notes', 'hide');
-    updateStatus('Logged out.');
-    focus('user');
-  },
-};
-
-addEventListener('click', event => {
-  if(event.target.type !== 'button' && event.target.type !== 'submit') {
-    return;
-  }
-
-  event.preventDefault();
-  switch (event.target.id) {
-    case 'login':
-      Buttons.login();
-      break;
-    case 'load':
-      Buttons.load();
-      break;
-    case 'save':
-      Buttons.save();
-      break;
-    case 'clear':
-      Buttons.clear();
-      break;
-    case 'logout':
-      Buttons.logout();
-      break;
-  }
-});
-
-addEventListener('submit', event => {
-  event.preventDefault();
-  switch (event.target.id) {
-    case 'login':
-      Buttons.login();
-      break;
-  }
-});
